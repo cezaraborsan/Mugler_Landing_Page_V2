51373a90bcafede6c9eaef99c8c06d495882e52d
Cezara Borsan <cezarabo@gmail.com>
2022-09-27 13:55:38 +0300

    header images update

--- footer.js
+++ footer.js
@@ -8,7 +8,7 @@ class Footer extends HTMLElement {
    <footer>
      <div class="footer-content">
        <div class="footer-logo">
-         <img src="./images/mugler_logo.svg" alt="MUGLER" />
+         <img src="./images/mugler-logo.svg" alt="MUGLER" />
        </div>
        <nav class="footer-menu">
          <div class="footer-col">
